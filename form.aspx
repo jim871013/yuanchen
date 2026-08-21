3d8d49fa6e43c74f677c9d595f8e83f07e1a8ac8
jim871013 <69022250+jim871013@users.noreply.github.com>
2026-08-21 17:03:49 +0800

    Add files via upload

---
+++ form.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/web/YuanChengMP.master" AutoEventWireup="true" CodeFile="form.aspx.cs" Inherits="form" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    
+        <div class="col-lg-6 grid1 mt-lg-0 mt-4 pl-lg-5" style="font-size:30px">
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            
+        </div>
+</asp:Content>
+
---
+++ form.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class form : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string outstr = "表單輸入完成：<br />";
+        string Name = Request.QueryString["Sname"].ToString();
+        string Company = Request.QueryString["Scompany"].ToString();
+        string Email = Request.QueryString["Semail"].ToString();
+        string Phone = Request.QueryString["Sphone"].ToString();
+        string Text = Request.QueryString["Stext"].ToString();
+
+        outstr += "公司名稱：" + Company + "<br />聯絡人：" + Name + "<br />電子信箱：" + Email + "<br />手機號碼：" + Phone + "<br />詢問內容：<br />" + Text ;
+
+        Label1.Text = outstr ;
+    }
+}
